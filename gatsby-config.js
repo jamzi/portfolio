8836ef4a32940fd859f501397c044aef31e42835
Janez Cadez <janez.cadez007@gmail.com>
2020-05-17 18:11:59 +0200

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-166918906-1`,
       },
     },
     `gatsby-plugin-feed`,
