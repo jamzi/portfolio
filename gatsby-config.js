a7e87f0e09968a734fdd9b4023989f84401adf31
Janez Cadez <janez.cadez007@gmail.com>
2022-04-02 15:05:29 +0200

    change name of the manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,7 +109,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `Janez Čadež Portfolio`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
